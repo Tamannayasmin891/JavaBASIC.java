8fa1034c5aad95a51e41f121ee052e6e84d83213
Tamanna Yasmin Shimu <tamannayasmin5050@gmail.com>
2025-08-19 23:38:34 +0600

    Create identifiers.java

---
+++ identifiers.java
@@ -0,0 +1,17 @@
+public class identifiers {
+
+public static void main(String[] args) {
+
+// Good
+
+int minutes PerHour 60;
+
+// OK, but not so easy to understand what m actually is
+
+int m = 60;
+
+System.out.println(minutes PerHour);
+
+System.out.println(m);
+ }
+}
