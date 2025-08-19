d27bc238780165669583310df94f31a367cfe493
Tamanna Yasmin Shimu <tamannayasmin5050@gmail.com>
2025-08-19 23:17:38 +0600

    Create DoubleQuotes.java

---
+++ DoubleQuotes.java
@@ -0,0 +1,9 @@
+public class public class DoubleQuotes{
+
+public static void main(String[] args) {
+
+System.out.println("This sentence will work!");
+
+System.out.println(This sentence will produce an error);
+ }
+}
