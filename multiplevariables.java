b2ad4ee1fa497ea4815744d4f63cfa32dae698f6
Tamanna Yasmin Shimu <tamannayasmin5050@gmail.com>
2025-08-21 09:03:51 +0600

    Update multiplevariables.java

--- multiplevariables.java
+++ multiplevariables.java
@@ -2,11 +2,11 @@ public class multiplevariables {
 
 public static void main(String[] args)
 
-int x = 5,
+int x = 5;
 
-int y = 6,
+int y = 6;
 
-int z = 50,
+int z = 50;
 
 System.out.println(x + y + z);
 
