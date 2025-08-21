e621294bd99ad76c44ff77759a61e5b19a225ed6
Tamanna Yasmin Shimu <tamannayasmin5050@gmail.com>
2025-08-21 09:09:52 +0600

    Update PrintVariables.java

--- PrintVariables.java
+++ PrintVariables.java
@@ -2,10 +2,10 @@ public class PrintVariables {
 
 public static void main(String[] args) {
 
-int x = 5
+int x = 5;
 
-int y = 6
+int y = 6;
 
-System.out.println 1(x + y) // Print the value of x + y
+System.out.println (x + y) // Print the value of x + y
  }
 }
